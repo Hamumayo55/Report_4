adefb4995c30f5fc55b60326c4684a78d5dcd245
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2017-11-19 17:17:27 +0900

    Step3:APIリファレンスを参照してみよう。

--- src/Main/jp/ac/uryukyu/ie/e175769/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e175769/Main.java
@@ -3,8 +3,9 @@ package jp.ac.uryukyu.ie.e175769;
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            double value = Double.parseDouble(str);
+            //System.out.println(str.length());
         }
         catch(NullPointerException e){
             System.out.println(e.toString()+"が発生");
